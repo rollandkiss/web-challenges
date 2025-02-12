74d5fd974330e2b1db4e6fd2992190fa231ccd31
Rolland Kiss <me@rollandkiss.com>
2025-02-12 16:42:00 +0100

    update challenge exercise - dark.mode

--- js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+document.querySelector('[data-js="dark-mode-button"]').addEventListener("click", () => {
+    bodyElement.classList.add("dark");
+    console.dir(bodyElement.classList);
+});
+
+document.querySelector('[data-js="light-mode-button"]').addEventListener("click", () => {
+    bodyElement.classList.remove("dark");
+    console.dir(bodyElement.classList);
+});
+
+document.querySelector('[data-js="toggle-button"]').addEventListener("click", () => {
+    bodyElement.classList.toggle("dark");
+    console.dir(bodyElement.classList);
+});
