7394143ff1378945b9d6ed53da38a9519e1489ab
Rolland Kiss <me@rollandkiss.com>
2025-02-12 11:56:34 +0100

    update challenge exercise tip calculator

--- js-basics/tip-calculator/js-basics-and-variables_tip-calculator/index.js
+++ js-basics/tip-calculator/js-basics-and-variables_tip-calculator/index.js
@@ -14,3 +14,12 @@ based on the cost of the meal and the percentage of the tip.
 
 4. Log a message to the console that includes the meal cost, tip amount, and total cost.
 */
+const mealCost = 20;
+const tipPercent = 0.1;
+
+let tipAmount = mealCost * tipPercent;
+let totalCost = mealCost + tipAmount;
+
+console.log("Meal Costs:", mealCost);
+console.log("Tip Amount:", tipAmount);
+console.log("Total Costs:", totalCost);
