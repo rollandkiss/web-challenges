5b2773d517a21a895eb57eb987e9ee9543599a74
Rolland Kiss <me@rollandkiss.com>
2025-02-17 15:19:17 +0100

    corrected the path

--- html-forms/js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++
@@ -1,8 +0,0 @@
-console.clear();
-
-let inputField = document.querySelector('[data-js="first-input"]');
-const buttonUpper = document.querySelector('[data-js="button-uppercase"]');
-
-buttonUpper.addEventListener("click", () => {
-  inputField.value = inputField.value.toUpperCase();
-});
