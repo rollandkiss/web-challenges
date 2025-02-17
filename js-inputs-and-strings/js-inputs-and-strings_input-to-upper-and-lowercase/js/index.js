3b6cd19c81294d8c54b051f359878cfc0a9a41c3
Rolland Kiss <me@rollandkiss.com>
2025-02-17 15:42:45 +0100

    update the challenge exercise >upper-and-lowercase

--- js-inputs-and-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -2,7 +2,12 @@ console.clear();
 
 const input = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const downcaseButton = document.querySelector('[data-js="button-downcase"]');
 
 uppercaseButton.addEventListener("click", () => {
   input.value = input.value.toUpperCase();
+});
+
+downcaseButton.addEventListener("click", () => {
+  input.value = input.value.toLowerCase();
 });
