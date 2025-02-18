e4c26df88d2f105cf2776c08da0a4b60cb8d4560
Rolland Kiss <me@rollandkiss.com>
2025-02-18 16:22:03 +0100

    update challenge exercise >social-media-post

--- js-createelement/js-createelement_social-media-post/js/index.js
+++ js-createelement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,30 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const newArticle = document.createElement("article");
+const newParagraph = document.createElement("p");
+const newFooter = document.createElement("footer");
+const newSpan = document.createElement("span");
+const newButton = document.createElement("button");
+
+// New Element via DOM and createElement
+document.body.append(newArticle);
+newArticle.classList = "post";
+
+newArticle.append(newParagraph);
+newParagraph.classList.add("post__content"); // übergabe des wertes mittels add-methode
+newParagraph.textContent = "Hokus Pokus Fidibus , drei mal schwarzer Kater";
+
+newArticle.append(newFooter);
+newFooter.classList = "post__footer";
+
+newFooter.append(newSpan);
+newSpan.classList = "post__username";
+newSpan.textContent = "die grüne Hexe";
+
+newFooter.append(newButton);
+newButton.setAttribute("classList", "post__button BLABLA"); // mehrer werte übergeben
+newButton.type = "button";
+newButton.setAttribute("data-js", "like-button"); // zuweisung des "data-js"
+newButton.textContent = "♥ Like";
+newButton.addEventListener("click", handleLikeButtonClick); // event listener dem neuen button zuweisen
