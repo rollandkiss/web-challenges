a6aee199dce30fe6f1d3a8a75a71606ddfa5e120
Rolland Kiss <me@rollandkiss.com>
2025-02-18 15:32:30 +0100

    update the challenge exercise >toast-message

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,15 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  let newMessage = document.createElement("li");
+  // toastContainer.append(newMessage); // placing the element as lastChild
+  // toastContainer.lastChild.textContent = "I'm a Toaster Message -appended" // lastChild is the position of the newly created element
+
+  toastContainer.prepend(newMessage); // placing the element as firstChild
+  toastContainer.firstChild.textContent = "I'm a Toaster Message -prepended" // firstChild is the position of the newly created element
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
