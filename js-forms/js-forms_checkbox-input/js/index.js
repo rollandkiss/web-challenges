8cefd956bf3062f4fd4b2e77ca2d03db37a1464e
Rolland Kiss <me@rollandkiss.com>
2025-02-18 12:59:43 +0100

    update challenge exercise >checkbox-input

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,9 +17,41 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  if (!tosCheckbox.checked) {
+    showTosError();
+    hideSubmissionMessage();
+    return;
+  }
+  showSubmissionMessage();
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  // alert("Form submitted");
+});
+
+// New listener to disable/enable the message text
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+    return;
+  }
+  showTosError();
+});
+
+// New functions to enable/disable the visibility of the submission DIV
+function showSubmissionMessage() {
+  successMessage.removeAttribute("hidden");
+  successMessage.setAttribute("visible", "");
+}
+
+function hideSubmissionMessage() {
+  successMessage.removeAttribute("visible");
+  successMessage.setAttribute("hidden", "");
+}
+
+// Versuch das bei änderung des formulars die message wieder versteckt wird
+form.addEventListener("change", () => {
+  if (successMessage.attributes === "visible") {
+    hideSubmissionMessage();
+  }
 });
