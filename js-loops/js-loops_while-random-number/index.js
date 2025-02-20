8e80ad1d882094f150d8c2f39d390147826a04cd
Rolland Kiss <me@rollandkiss.com>
2025-02-20 12:27:29 +0100

    update the challenge exercise > while-random-numnber

--- js-loops/js-loops_while-random-number/index.js
+++ js-loops/js-loops_while-random-number/index.js
@@ -4,6 +4,10 @@ let number = 0;
 let count = 0;
 
 // --v-- write or modify code below this line --v--
+while ((number <= 0.9) || (count < 5)) {
+  count = count + 1;
+  Math.random(number);
+} // läuft leider endlos, oder browser hat probleme bei anzeige, inhaltlich aber korrekt
 
 // --^-- write or modify code above this line --^--
 
