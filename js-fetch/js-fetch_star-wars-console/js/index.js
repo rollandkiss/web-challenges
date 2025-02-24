c9011d25b4630479be24ad3e02b93b7f9a3ba23b
Rolland Kiss <me@rollandkiss.com>
2025-02-24 12:07:04 +0100

    update challenge exercise >star-wars-console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,14 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+  console.log(data.results);
+  
+  // Eye color of R2-D2 > object:results > array: id=2 > object:eye_color
+  console.log(data.results[2]["eye_color"]);
+}
 
-fetchData();
+fetchData(url);
