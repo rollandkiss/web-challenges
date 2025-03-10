2e20f501d9f32ec283f228c15353a1236281cec4
Rolland Kiss <me@rollandkiss.com>
2025-03-10 15:53:06 +0100

    update exercise > arrays-fruits

--- react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,39 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍏 Apple",
+      color: "green",
+    },
+    {
+      id: 1339,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+    {
+      id: 1340,
+      name: "🫐 Blue Berries",
+      color: "purple",
+    },
+    {
+      id: 1341,
+      name: "🥭 Mango",
+      color: "red",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => {
+        return <Card key={fruit.id} name={fruit.name} color={fruit.color} />; // zu beginn des Ausführungsblocks ist ein RETURN zu verwenden
+      })}
     </div>
   );
 }
--- react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,9 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ id, name, color }) {
+  return (
+    <p className="card" key={id} style={{ backgroundColor: color }}>
+      {name}
+    </p>
+  );
 }
