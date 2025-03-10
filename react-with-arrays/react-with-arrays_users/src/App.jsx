6d41942b99ff3d497d8b79cd0afade675b892500
Rolland Kiss <me@rollandkiss.com>
2025-03-10 16:36:43 +0100

    update exercise > arrays-users

--- react-with-arrays/react-with-arrays_users/src/App.jsx
+++ react-with-arrays/react-with-arrays_users/src/App.jsx
@@ -9,7 +9,10 @@ export default function App() {
     <main className="app">
       <Title text="ClientBoard" />
       <div className="app__card-grid">
-        <Card user={USERS[0]} />
+        {/* <Card user={USERS[0]} /> */}
+        {USERS.map((user) => {
+          return <Card key={user.id} user={user} />;
+        })}
       </div>
     </main>
   );
--- react-with-arrays/react-with-arrays_users/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_users/src/components/Card.jsx
@@ -1,12 +1,19 @@
 import "./Card.css";
 import Tag from "./Tag";
 import Button from "./Button";
-function Card({ user }) {
+function Card({ id, user }) {
+  // console.log(user);
   return (
     <article className="card">
       <h2>{user.name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+        {/* <Tag tag={user.roles[0]} /> */}
+        {user.roles.map((role) => {
+          if (role !== "admin") {
+            return <Tag key={role} tag={role} className="tag" />;
+          }
+          return <Tag key={role} tag={role} className="tag tag--highlight" />;
+        })}
       </ul>
       <p>{user.about}</p>
       <Button>edit</Button>
