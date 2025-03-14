7ee9d514c779d89526139369c9ffe94aceed9080
Rolland Kiss <me@rollandkiss.com>
2025-03-14 10:57:30 +0100

    update exercise > created function based on test scripts

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  }
+  if (animal === "dogs") {
+    return `I like ${animal}!`;
+  }
+  return "I do not like animals at all!";
+}
